a1856e4792ad70fe7d77b4b9086b125266b49faf
Lifeissimple-zxc <mararkarp@gmail.com>
2026-04-27 18:37:45 +0200

    eval runner scaffolded

--- typescript/scripts/010_prompt_eval_1.ts
+++ typescript/scripts/010_prompt_eval_1.ts
@@ -1,18 +1,68 @@
-const getPrompt = (task: string) => {
-  `Please provide a solution to the following task:\n${task}`
+import { MessageParam } from "@anthropic-ai/sdk/resources"
+import { readFile } from "node:fs/promises"
+import { ClientWithMessageHistory } from "../anthropic/client.js"
+import { Anthropic } from "@anthropic-ai/sdk"
+
+interface Task {
+  task: string
 }
 
-const tasks = [
-  {
-    task: "Create a python function to extract the AWS account ID from ARN"
-  },
-  {
-    task: "Write a JSON policy document that allows read-only access to a specific S3 bucket"
-  },
-  {
-    task: "Provide a regex for validating an AWS iam"
-  },
-  {
-    task: "Provide a CLI command to trigger a elastic AWS container to be redeployed"
+interface EvalResult {
+  output: string
+  testCase: string
+  score: number
+}
+
+const client = new ClientWithMessageHistory(
+  new Anthropic(),
+  "claude-sonnet-4-5",
+  1000,
+)
+
+const getPrompt = (task: string) => {
+  return `Please provide a solution to the following task:\n${task}`
+}
+
+async function runTestCase(testCase: string): Promise<EvalResult> {
+  // get response from claude
+  console.log(`evaling testcase ${testCase}`)
+  const output = await client.chat(
+    [
+      {
+        content: getPrompt(testCase),
+        role: "user"
+      }
+    ]
+  )
+  // TODO grading
+  const score = 10
+  
+  return {
+    output: output,
+    testCase: testCase,
+    score: score
   }
-]
+}
+
+async function runEvals(tasks: Task[]): Promise<EvalResult[]> {
+  return Promise.all(tasks.map(t => runTestCase(t.task)))
+}
+
+const text = await readFile("data/tasks.json", "utf-8")
+console.log("loaded file")
+const tasks: Task[] = JSON.parse(text)
+console.log(`${tasks.length} task(s) found`)
+
+
+
+
+// eval loop
+console.log("starting eval loop")
+
+
+
+console.log("################################")
+const evalResults = await runEvals(tasks)
+console.log("evals completed")
+console.log(evalResults)
+
